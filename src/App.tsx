333124f2cfd31ec04c77b31ec8d75866414a1dc4
VovaLyapota <rimidalvatopayl@gmail.com>
2024-02-26 17:50:50 +0200

    initialize project, add tailwind and castomize shadsn-ui

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      react app
+      <Button>Add button</Button>
+    </>
+  );
+}
+
+export default App;
